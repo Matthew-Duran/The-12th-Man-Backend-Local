85c669c117fa538d931fce8614a8a38d7840097b
Matthew Duran <Matthew.q.duran@gmail.com>
2025-09-10 00:57:04 -0400

    Framework for Premier League Website

---
+++ src/main/java/com/pl/The_12th_Man/The12thManApplication.java
@@ -0,0 +1,13 @@
+package com.pl.The_12th_Man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class The12thManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(The12thManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/The_12th_Man/The12thManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.The_12th_Man;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class The12thManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
